9944c6e69ead538c06aa47c495261ceecf77c46c
cheapjack <ross@cheapjack.org.uk>
2019-12-10 19:46:35 +0000

    made main.py even simpler to get started with a voltage divider

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-import machine
 from machine import Signal, Pin, ADC
 import time
 
 ledPin2 = machine.Pin(2, machine.Pin.OUT)
 Led2 = Signal(ledPin2, invert=True)
-
-Led2.off()
-time.sleep(0.5)
-Led2.on()
 
 adc = ADC(0)
 
