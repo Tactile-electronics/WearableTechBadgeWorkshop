0c196d1e60b47fc3f4423bc8424606d79f8c5a46
@cheapjack <cheapjack@protonmail.com>
2020-02-01 11:38:18 +0000

    Update adc_main.py
    added some comments

--- adc_main.py
+++ adc_main.py
@@ -5,9 +5,9 @@ ledPin2 = machine.Pin(2, machine.Pin.OUT)
 Led2 = Signal(ledPin2, invert=True)
 
 adc = ADC(0)
-
+sensor_max = 500 # This is the max reading for my sensor, you'll need to find your max reading
 while True:
-    stretch = 500 - (adc.read() *10)
+    stretch = sensor_max - (adc.read() *10) #assign stretch to the maximum less our sensor reading
     Led2.on()
     time.sleep_ms(stretch)
     Led2.off()
